3905a93934914e7508c0c23e7140d40c1a705f86
joe <shangchuanren@gmail.com>
2012-11-21 16:30:28 +0800

    Implement the control module of overall logic and related test bench.

--- logic/control.v
+++ logic/control.v
@@ -1,3 +1,185 @@
-// FileName: control.v
-// Author: Joe Shang
+// Name: control
 // Description:
+// Author: Joe Shang
+
+module control(reset, clock, set, alerm, alerm_switch, minute_set, hour_set,
+			   time_data, alerm_equal);
+
+	input reset;
+	input clock;
+	input set;
+	input alerm;
+	input alerm_switch;
+	input minute_set;
+	input hour_set;
+	output alerm_equal;
+	output [time_width-1:0] time_data;
+
+	reg [1:0] current_state;
+	reg [1:0] next_state;
+
+	reg output_sel;
+	reg timer_mode;
+	reg alerm_open;
+
+	wire alerm_enable;
+	wire alerm_minute_set;
+	wire alerm_hour_set;
+	wire timer_minute_set;
+	wire timer_hour_set;
+
+	wire [5:0] alerm_second;
+	wire [5:0] alerm_minute;
+	wire [5:0] alerm_hour;
+	wire [5:0] timer_second;
+	wire [5:0] timer_minute;
+	wire [5:0] timer_hour;
+
+	wire [time_width-1:0] alerm_output;
+	wire [time_width-1:0] timer_output;
+
+	parameter timer_state = 0;
+	parameter set_state = 1;
+	parameter alerm_state = 2;
+
+	parameter time_width = 18;
+	
+	// TEST
+	parameter second_cnt = 4;
+
+	// FSM:Changing state.
+	always @(posedge clock or negedge reset)
+	begin
+		if (!reset)
+			current_state <= timer_state;
+		else
+			current_state <= next_state;
+	end
+
+	// FSM:Calculating next state by current state and input.
+	always @(current_state or set or alerm)
+	begin
+		case (current_state)
+
+		timer_state:
+			if (!set && !alerm)
+				next_state <= timer_state;
+			else
+			begin
+				if (alerm)
+					next_state <= alerm_state;
+				if (set)
+					next_state <= set_state;
+			end
+
+		set_state:
+			if (set)
+				next_state <= set_state;
+			else
+			begin
+				if (alerm)
+					next_state <= alerm_state;
+				else
+					next_state <= timer_state;
+			end
+		
+		alerm_state:
+			if (alerm)
+				next_state <= alerm_state;
+			else
+			begin
+				if (set)
+					next_state <= set_state;
+				else
+					next_state <= timer_state;
+			end	
+
+		endcase
+	end
+
+	// FSM:Output control signal by current state.
+	always @(posedge clock or negedge reset)
+	begin
+		if (!reset)
+		begin
+			output_sel <= 1;
+			timer_mode <= 1;
+			alerm_open <= 1;
+		end
+		else
+		begin
+			case (current_state)
+				
+				timer_state:
+				begin
+					output_sel <= 1;	// Output Timer/Set register data.
+					timer_mode <= 1;	// Timer/Set register perform at timer mode.
+					alerm_open <= 1;	// Alerm comparation open.
+				end
+
+				set_state:
+				begin
+					output_sel <= 1;	// Output Timer/Set register data.
+					timer_mode <= 0;	// Timer/Set register perform at set mode. 
+					alerm_open <= 0;	// Alerm comparation close.
+				end
+
+				alerm_state:
+				begin
+					output_sel <= 0;	// Output Alerm register data.
+					timer_mode <= 1;	// Timer/Set register perform at timer mode.
+					alerm_open <= 0;	// Alerm comparation close.
+				end
+
+			endcase
+		end
+	end
+
+	// sel = 1, external set signal access in timer register.
+	assign timer_minute_set = output_sel ? minute_set : 0;
+	assign timer_hour_set   = output_sel ? hour_set   : 0;
+	// sel = 0, external set signal access in alerm register.
+	assign alerm_minute_set = output_sel ? 0 : minute_set;
+	assign alerm_hour_set	= output_sel ? 0 : hour_set;
+
+	// Two-in-One selector.
+	// sel = 1, output timer data.
+	// sel = 0, output alerm data.
+	assign time_data = output_sel ? timer_output : alerm_output; 
+	
+	assign timer_output = {timer_hour, timer_minute, timer_second};
+	assign alerm_output = {alerm_hour, alerm_minute, alerm_second};
+
+	// Alerm comparator.
+	// alerm_open = 1, open alerm comparation.
+	// alerm_open = 0, close alerm comparation.
+	assign alerm_enable = alerm_open ? alerm_switch : 0;
+
+	reg_alerm alerm_data_reg(.reset(reset),
+							 .clock(clock),
+							 .minute_set(alerm_minute_set),
+							 .hour_set(alerm_hour_set),
+							 .second_data(alerm_second),
+							 .minute_data(alerm_minute),
+							 .hour_data(alerm_hour));
+
+	reg_timer timer_data_reg(.reset(reset),
+							 .clock(clock),
+							 .mode(timer_mode),
+							 .minute_set(timer_minute_set),
+							 .hour_set(timer_hour_set),
+							 .second_data(timer_second),
+							 .minute_data(timer_minute),
+							 .hour_data(timer_hour));
+
+	alerm_comp  comp_module(.reset(reset),
+		   					.clock(clock),	
+							.timer_data(timer_output),
+							.alerm_data(alerm_output),
+							.alerm_enable(alerm_enable),
+							.alerm_output(alerm_equal));
+	// TEST
+	defparam timer_data_reg.second_cnt = second_cnt;
+	defparam comp_module.second_cnt = second_cnt;
+
+endmodule
---
+++ test_bench/test_control.v
@@ -0,0 +1,129 @@
+// Name: test_control
+// Description:
+// Author: Joe Shang
+
+`timescale 1ns / 1ns
+
+module test_control;
+
+	reg reset;
+	reg clock;
+	reg set;
+	reg alerm;
+	reg alerm_switch;
+	reg minute_set;
+	reg hour_set;
+	
+	wire [23:0] time_data;
+	wire alerm_equal;	
+
+	`define clock_period 2
+	`define second_period 4
+
+	control test_module(.reset(reset),
+					  	.clock(clock),
+					  	.set(set),
+					  	.alerm(alerm),
+					  	.alerm_switch(alerm_switch),
+					  	.minute_set(minute_set),
+					  	.hour_set(hour_set),
+					  	.time_data(time_data),
+					  	.alerm_equal(alerm_equal));
+	defparam test_module.second_cnt = `second_period;
+
+	initial
+	begin
+
+		$dumpfile("test_control.vcd");
+		$dumpvars(0, test_control);
+
+		//$monitor("  %d:%d:%d -> %d", time_data[23:16], time_data[15:8], time_data[7:0], alerm_equal);
+
+		reset = 0;
+		clock = 1;
+		set = 0;
+		alerm = 0;
+		alerm_switch = 1;
+		minute_set = 0;
+		hour_set = 0;
+
+		$display("Test control module\n");
+		#(`clock_period * 4)
+
+		reset = 1;
+		$display("Start to run, at Timer mode");
+		#(`clock_period * `second_period * 60 * 5)
+
+		set = 1;
+		$display("Enter into Set mode");
+		#(`clock_period * `second_period * 10)
+
+		minute_set = 1;
+		hour_set = 0;
+		$display("External minute set signal valid");
+		#(`clock_period * 70)
+
+		minute_set = 0;
+		hour_set = 1;
+		$display("External hour set signal valid");
+		#(`clock_period * 30)
+
+		alerm = 1;
+		hour_set = 0;
+		$display("Want to enter Alerm mode");
+		#(`clock_period * `second_period * 10)
+
+		set = 0;
+		$display("Enter into Alerm mode");
+		#(`clock_period * `second_period * 10)
+
+		minute_set = 1;
+		hour_set = 0;
+		$display("External minute set signal valid, make alerm one minute early than timer");
+		#(`clock_period * 76)
+
+		minute_set = 0;
+		hour_set = 1;
+		$display("External hour set signal valid");
+		#(`clock_period * 30)
+
+		alerm = 0;
+		hour_set = 0;
+		$display("Return to timer mode and wait for alerm(alerm 60s then dismiss)");
+		#(`clock_period * `second_period * 60 * 3)
+
+		alerm = 1;
+		minute_set = 1;
+		hour_set = 0;
+		$display("Set alerm time one minute early than timer again.");
+		#(`clock_period * 63)
+
+		alerm = 0;
+		$display("Return to timer mode and wait for alerm");
+		#(`clock_period * `second_period * 60 * 1)
+
+		alerm_switch = 0;
+		$display("Close alerm switch");
+		#(`clock_period * 60)
+		
+		alerm = 1;
+		minute_set = 1;
+		hour_set = 0;
+		$display("Set alerm time one minute early than timer again.");
+		#(`clock_period * 62)
+
+		alerm = 0;
+		$display("Return to timer mode and wait for alerm");
+		#(`clock_period * `second_period * 60 * 1)
+
+		alerm_switch = 1;
+		$display("Close alerm switch");
+		#(`clock_period * 60)
+
+		$finish;
+	end
+
+	always #1
+		clock = ~clock;
+
+endmodule
